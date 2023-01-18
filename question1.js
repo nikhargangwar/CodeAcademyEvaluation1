527132c564e724b6adee2f2f7916c81bffe9f8ae
nikhar <nikhar.gangwar13@gmail.com>
2023-01-18 15:29:22 +0530

    feat: add more test cases

--- question1.js
+++ question1.js
@@ -1,35 +1,40 @@
 const CalculateScoreOfGame = (arr) => {
 
-    let strikeScore = 0, spareScore = 0, openScore = 0;
-    let strikeFlag=0,spareFlag=0;
-    forEach((frame) => {
-        if (frame[0] === 10) {
+    let strikeScore = 0, spareScore = 0, openScore = 0
+    let strikeFlag=0,spareFlag=0
+
+    arr.forEach((frame) => {
+        //console.log(frame)
+        if (frame[0] ===10) {
             strikeScore += 10
-            strikeFlag = 1;
+            strikeFlag = 1
         }
         else if (frame[0] + frame[1] === 10) {
             if (strikeFlag === 1) {
-                strikeScore = frame[0] + frame[1];
-                strikeFlag = 0;
+                strikeScore += frame[0] + frame[1]
+                strikeFlag = 0
             }
             spareScore += 10
-            spareFlag = 1;
+            spareFlag = 1
         }
         else {
+            console.log(frame)
             if (strikeFlag === 1) {
-                strikeScore = frame[0] + frame[1];
-                strikeFlag = 0;
+                strikeScore += frame[0] + frame[1]
+                strikeFlag = 0
             }
             if (spareFlag === 1) {
                 spareScore += frame[0]
                 spareFlag = 0
             }
-            openScore = frame[0] + frame[1];
+            openScore += frame[0] + frame[1]
         }
 
     })
 
     return spareScore+strikeScore+openScore
 }
+
+//console.log(CalculateScoreOfGame([[1,2],[3,4],[5,4],[4,3],[3,3],[2,3],[4,5],[5,2],[3,3],[3,2]]))
 
 module.exports = {CalculateScoreOfGame}
--- question1.test.js
+++ question1.test.js
@@ -1,10 +1,29 @@
-const { CalculateScoreOfGame } = require('./question');
+const { CalculateScoreOfGame } = require('./question1');
 describe('Testing Score of a bowling game', () =>
   describe('testing score', () => {
-    it('Should double the elements when the input is an array', () => {
-      const result = CalculateScoreOfGame([1,2],[3,4],[5,4],[4,3],[3,3][2,3],[4,5],[5,2],[3,3],[3,2]);
+    it('should return score when input is all open frames', () => {
+      const result = CalculateScoreOfGame([[1,2],[3,4],[5,4],[4,3],[3,3],[2,3],[4,5],[5,2],[3,3],[3,2]]);
       expect(result).toEqual(64);
     })
+
+    it('should return score when input consist of spare frames', () => {
+      const result = CalculateScoreOfGame([[1,2],[3,4],[5,4],[5,5],[3,3],[2,3],[4,5],[5,2],[3,3],[3,2]]);
+      expect(result).toEqual(70);
+    })
+
+    it('should return score when input consist of strike frames', () => {
+      const result = CalculateScoreOfGame([[1,2],[3,4],[5,4],[5,5],[3,3],[2,3],[4,5],[10,0],[3,3],[3,2]]);
+      expect(result).toEqual(79);
+    })
     
+    it('should return score when input consist of open frames', () => {
+      const result = CalculateScoreOfGame([[3, 6],[ 3, 6],[3, 6], [3, 6], [3, 6], [3, 6], [3, 6], [3, 6], [3, 6], [3, 6]]);
+      expect(result).toEqual(90);
+    })
+    
+    it('should return score when input consist of strike frames', () => {
+      const result = CalculateScoreOfGame([[6, 4], [3, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]);
+      expect(result).toEqual(16);
+    })
   })
 );
